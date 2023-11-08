2d483c35da17eb58a7b1e018660f38bb9de6a9ed
quanhoang191003 <quanhoang191003@gmail.com>
2023-11-08 17:41:17 +0700

    new

--- app/models/db.js
+++ app/models/db.js
@@ -1,11 +1,20 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const dbConfig = require("../config/db.config.js");
 
-var connection = mysql.createPool({
+var connection = mysql.createConnection({
   host: dbConfig.HOST,
   user: dbConfig.USER,
   password: dbConfig.PASSWORD,
   database: dbConfig.DB
 });
+
+connection.connect((err) => {  
+  if(!err) {  
+      console.log("Db Connection Succeed");  
+  }  
+  else{  
+      console.log("Db connect Failed \n Error :" + JSON.stringify(err,undefined,2));  
+  }  
+});  
 
 module.exports = connection;
